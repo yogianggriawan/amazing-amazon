feec6e8caf701bc6027e93ab1b5a6675895f3414
yogianggriawan <yogi.anggriawan@live.com>
2017-04-26 14:31:45 +0700

    add auto sitemap amazon

--- amazing-amazon/amazon-suggestion.php
+++
@@ -1,5 +0,0 @@
-https://completion.amazon.com/search/complete?method=complete&q=xiaomi&search-alias=aps&conf=1&sv=android-mshop&l=en&mkt=1&client-id=unknown
-<div class="related"> Related:
-<span style="margin-left: 12px;">
-<a href="../apple-laptop">
-<strong class="keywords">apple</strong> laptop</a></span> </div>
